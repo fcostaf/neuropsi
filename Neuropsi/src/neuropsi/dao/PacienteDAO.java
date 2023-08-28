a2b039d72d24ef2b91465c2a85c30256d710cc3a
fcostaf <fcostaf@yahoo.com.br>
2023-08-28 00:32:39 -0300

    Alterar,excluir,listar

--- Neuropsi/src/neuropsi/dao/PacienteDAO.java
+++ Neuropsi/src/neuropsi/dao/PacienteDAO.java
@@ -40,4 +40,95 @@ public class PacienteDAO {
             }catch(SQLException e){e.printStackTrace();}
         }
     }
+    
+    public void alterarPaciente(Paciente p) throws ExceptionDAO{
+        String sql="update paciente set nome=?,sexo=?,lateralidade=?,dn=?,ln=?,escolaridade=?,curso=?,profissao=?,estado_civil=?,filhos=?,comentario=?";
+        PreparedStatement stmt=null;
+        Connection connection=null;
+        try{
+            connection=new Conexao().getConnection();
+            stmt=connection.prepareStatement(sql);
+            stmt.setString(1, p.getNome());
+            stmt.setString(2,p.getSexo());
+            stmt.setString(3,p.getLateralidade());
+            stmt.setString(4,p.getDn());
+            stmt.setString(5,p.getLn());
+            stmt.setString(6,p.getEscolaridade());
+            stmt.setString(7,p.getCurso());
+            stmt.setString(8,p.getProfissao());
+            stmt.setString(9,p.getEstado_civil());
+            stmt.setString(10,p.getFilhos());
+            stmt.setString(11,p.getComentario());
+            stmt.execute();
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new ExceptionDAO("Erro ao alterar paciente: "+e);
+        }finally{
+            try {if(stmt!=null){stmt.close();}
+            }catch (SQLException e){e.printStackTrace();}
+            try {if(connection !=null){connection.close();}
+            }catch(SQLException e){e.printStackTrace();}
+        }
+    }
+    
+    public void excluirPaciente(int id) throws ExceptionDAO{
+        String sql="delete from paciente where id=?";
+        PreparedStatement stmt=null;
+        Connection connection=null;
+        try{
+            connection=new Conexao().getConnection();
+            stmt=connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.execute();
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new ExceptionDAO("Erro ao excluir paciente: "+e);
+        }finally{
+            try {if(stmt!=null){stmt.close();}
+            }catch (SQLException e){e.printStackTrace();}
+            try {if(connection !=null){connection.close();}
+            }catch(SQLException e){e.printStackTrace();}
+        }
+    }
+    
+    public ArrayList<Paciente> listarPaciente() throws ExceptionDAO{
+        ResultSet rs=null;
+        Connection conn=null;
+        PreparedStatement stmt=null;
+        Paciente p=null;
+        ArrayList<Paciente> listaDePacientes=null;
+        
+        try{
+            String sql="select * from paciente";
+            conn=new Conexao().getConnection();
+            stmt=conn.prepareStatement(sql);
+            rs=stmt.executeQuery();
+            if(rs!=null){
+                listaDePacientes=new ArrayList<>();
+                while(rs.next()){
+                    p=new Paciente();
+                    p.setIdpaciente(rs.getString("idpaciente"));
+                    p.setNome(rs.getString("nome"));
+                    p.setComentario(rs.getString("comentario"));
+                    p.setCurso(rs.getString("curso"));
+                    p.setDn(rs.getString("dn"));
+                    p.setEscolaridade(rs.getString("escolaridade"));
+                    p.setEstado_civil(rs.getString("estado_civil"));
+                    p.setFilhos(rs.getString("filhos"));
+                    p.setLateralidade(rs.getString("lateralidade"));
+                    p.setLn(rs.getString("lateralidade"));
+                    p.setProfissao(rs.getString("profissao"));
+                    p.setSexo(rs.getString("sexo"));
+                    listaDePacientes.add(p);}}
+                }catch(SQLException e) {e.printStackTrace();
+                        throw new ExceptionDAO("Erro ao listar paciente: "+e);
+                }finally{try{if(rs!=null){rs.close();}
+                        }catch(SQLException e){e.printStackTrace();}
+                        try{if(stmt!=null){stmt.close();}
+                        }catch(SQLException e){e.printStackTrace();
+                        }try{if(conn!=null){conn.close();}
+                        }catch(Exception e){
+                            e.printStackTrace();}
+            }return listaDePacientes;
+    }
 }
